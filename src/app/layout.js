0be9da9302b35d7d6ef9b4b5e901e0f407c7b726
Kshitij Dutt <kshtjdutt07@gmail.com>
2023-10-12 12:51:19 +0530

    index.css  added with reset modifications

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
+import styles from "@/styles/index.css";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <title>cricflix | Home of premium quality highlights</title>
       <body>{children}</body>
     </html>
   );
